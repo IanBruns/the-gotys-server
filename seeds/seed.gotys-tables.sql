aecbdcde98a30811ebf7ba5e60e0a398356b39f2
Ian Bruns <ian@rbruns.com>
2021-02-04 14:24:19 -0700

    created base seed table

--- seeds/seed.gotys-tables.sql
+++ seeds/seed.gotys-tables.sql
@@ -5,3 +5,22 @@ TRUNCATE
     games,
     users
     RESTART IDENTITY CASCADE;
+
+INSERT INTO users (user_name, password)
+VALUES
+('test_man', '$2a$12$Zl/Enp7x915KObPF8ehDbe9kGd3FmE.P1NvNmz01HfgHmZiNXXQc2'),
+('sonic', '$2a$12$vxvuspXDF5FhHnTtPc.cse7sWUmasmH8CJNdbk1JyHztzXtuFldnq');
+
+INSERT INTO games (game_name, assigned_user)
+VALUES
+('game 1', 1)
+('game 2', 1)
+('game 3', 2)
+('game 4', 2);
+
+INSERT INTO reviews (score, review, current_year, assigned_game)
+VALUES
+(4, 'Review of game 1', true, 1),
+(8, 'Review of game 2', no, 1),
+(10, 'Review of game 3', no, 1),
+(9, 'Review of game 4', yes, 1);
